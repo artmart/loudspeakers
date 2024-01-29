150a026d58209ba9b3466136bd801da9ce47f23b
artmart <artrmart@gmail.com>
2024-01-29 23:35:49 +0400

    uuu

--- backend/assets/AppAsset.php
+++ backend/assets/AppAsset.php
@@ -12,15 +12,15 @@ class AppAsset extends AssetBundle
     public $basePath = '@webroot';
     public $baseUrl = '@web';
     public $css = [
-        'vendor/bootstrap-daterangepicker/daterangepicker.css',
-        'vendor/bootstrap-select/css/bootstrap-select.min.css',
-        'vendor/fontawesome/css/font-awesome.min.css',
+        //'vendor/bootstrap-daterangepicker/daterangepicker.css',
+        //'vendor/bootstrap-select/css/bootstrap-select.min.css',
+        //'vendor/fontawesome/css/font-awesome.min.css',
         'css/site.css',
     ];
     public $js = [
-        'vendor/moment/min/moment.min.js',
-        'vendor/bootstrap-select/js/bootstrap-select.min.js',
-        'vendor/bootstrap-daterangepicker/daterangepicker.js'
+        //'vendor/moment/min/moment.min.js',
+        //'vendor/bootstrap-select/js/bootstrap-select.min.js',
+        //'vendor/bootstrap-daterangepicker/daterangepicker.js'
     ];
     public $depends = [
         'yii\web\YiiAsset',
