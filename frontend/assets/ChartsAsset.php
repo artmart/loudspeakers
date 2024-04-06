f4c21d79714b3c081d5fda6324d49b5c5d195752
artmart <artrmart@gmail.com>
2024-04-06 16:56:41 +0400

    chart

---
+++ frontend/assets/ChartsAsset.php
@@ -0,0 +1,29 @@
+<?php
+namespace frontend\assets;
+
+use yii\web\AssetBundle;
+
+class ChartsAsset extends AssetBundle
+{
+    public $basePath = '@webroot';
+    public $baseUrl = '@web';
+   /* public $css = [
+        //'css/site.css',
+        'vendor/font-awesome/css/font-awesome.min.css',
+        'vendor/iCheck/skins/flat/green.css',	
+        'build/css/custom.css',
+    ];*/
+    public $js = [
+        "vendor/chart.js/dist/Chart.bundle.min.js",
+        "vendor/highcharts/highcharts.js",
+        "vendor/highcharts/exporting.js",
+        "vendor/highcharts/export-data.js",
+    ];
+   /* public $depends = [
+        'yii\web\YiiAsset',
+        'yii\web\JqueryAsset',
+        'yii\bootstrap4\BootstrapAsset',
+        'yii\bootstrap4\BootstrapPluginAsset',
+    ];*/
+    public $jsOptions = [ 'position' => \yii\web\View::POS_HEAD ];
+}
